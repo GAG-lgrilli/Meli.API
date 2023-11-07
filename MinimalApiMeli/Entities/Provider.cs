3e21bc52b30357a94864be5d4336be0c697be499
Mauro Candia <mcandia@GAG.LOCAL>
2023-11-07 15:03:14 -0300

    MC:feat:news changes

--- MinimalApiMeli/Entities/Provider.cs
+++ MinimalApiMeli/Entities/Provider.cs
@@ -2,15 +2,8 @@
 
 public class Provider
 {
-/*    public Provider() { 
-    
-    Products_Provider = new HashSet<Products>();
-    
-    }*/
-
     public int id { get; set; }
 
     public string descripcion { get; set; }
 
-  //  public virtual ICollection<Products> Products_Provider { get; set; }
 }
