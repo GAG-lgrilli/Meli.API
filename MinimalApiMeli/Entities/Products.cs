0e38afcbde87a31855c03bdcf5ebedb7bf37af68
Mauro Candia <mcandia@GAG.LOCAL>
2023-11-08 08:15:52 -0300

    MC:feat:entity

---
+++ MinimalApiMeli/Entities/Products.cs
@@ -0,0 +1,11 @@
+﻿namespace MinimalApiMeli.Entities;
+
+public class Products
+{
+    public int id { get; set; }
+    public string description { get; set; }
+    public string categoryId { get; set; }
+    public int price { get; set; }
+    public int quantity { get; set; }
+    public string providerId { get; set; }
+}
